99cbdba4dabee5472aede64b5161b59b7e4fa14f
Karabo Rithuri <rithurikarabo@gmail.com>
2023-08-10 17:34:49 +0200

    scroll reveal

--- script.js
+++ script.js
@@ -21,3 +21,19 @@ darkmode.onclick = () => {
         document.body.classList.remove('active');
     }
 }
+// Scroll Reveal
+const sr = ScrollReveal ({
+    origin: 'top',
+    distance: '40px',
+    duration: 2000,
+    reset: true
+});
+
+
+sr.reveal(`.home-text, .home-img,
+            .about-img, .about-text,
+            .box, .s-box,
+            .btn, .connect-text,
+            .contact-box`, {
+    interval: 200
+})
